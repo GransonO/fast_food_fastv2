1993c00f332d08c36878d107084d2d3020bc0f1d
GransonO <oyombegranson@gmail.com>
2018-09-21 02:13:49 +0300

    [ft#160670462]Create all end points base

--- server/run.py
+++ server/run.py
@@ -1,10 +1,7 @@
 from flask import Flask
-from flask_restplus import Resource,Api
-from app import create_app
+from flask_restplus import Api
 
-flask_app = create_app('Developing')
-
-#from app.api.v1 import routes
+from app.api.v1 import routes
 
 if __name__ == '__main__':
-    flask_app.run()
+    routes.app.run()

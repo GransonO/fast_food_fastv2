20115f04037384b483fa510f081e1a6b7f46cccc
GransonO <oyombegranson@gmail.com>
2018-09-21 04:00:36 +0300

    [ft#160675064]Create post new order

--- server/app/api/v1/routes/__init__.py
+++ server/app/api/v1/routes/__init__.py
@@ -1,11 +1,14 @@
 from flask import Flask,Blueprint
-from flask_restplus import Resource,Api
+from flask_restplus import Resource,Api,fields
 from app import create_app      #Used . to import from a top level package
 from ..services.data_handler import DataSet  #package for data manipulations
 
 app = create_app('Developing')
 
 api = Api(app)
+post_order = api.model('Posting Order',{'name':fields.String('Name of the item'),'Description' : fields.String('Brief description of the item'),
+'quantity' : fields.Integer('Total count of items'),'price': fields.Integer('Selling price'),'vendor':fields.String('Name of Vendor'),
+'location':fields.String('Where located'),'image':fields.String('Your image url')})
 
 #For all Orders
 class All(Resource):
@@ -14,8 +17,13 @@ class All(Resource):
         result = DataSet.get_all_orders(self)
         return {'data': result}, 200
 
+    @api.expect(post_order)
     def post(self):
-        return '<h3>Post an entry</h3>'
+        '''Adds a new item to the items list'''
+        sent_data = api.payload
+        result = DataSet.add_new_entry(self,sent_data['name'],sent_data['Description'],sent_data['quantity'],sent_data['price'],sent_data['vendor'],sent_data['location'],sent_data['image'])
+        return {'data': result}
+        
 
 #For Specific
 class Specific(Resource):
--- server/app/api/v1/services/data_handler.py
+++ server/app/api/v1/services/data_handler.py
@@ -2,10 +2,29 @@ class DataSet():
 
     ORDERS = []
 
+#=========================Handles Administrators Transactions==================================
+ 
     def get_all_orders(self):
         '''Return all orders to the request'''
         if len(DataSet.ORDERS) < 1:
             return 'No Items present'
         else:
             return DataSet.ORDERS
-        
+
+    def add_new_entry(self,name,Description,quantity,price,vendor,location,image):
+        '''Add new item to list'''
+        item_id = len(DataSet.ORDERS) + 1
+        item = {
+            'id' : item_id,
+            'name' : name,
+            'description' : Description,
+            'quantity' : quantity,
+            'price' : price,
+            'vendor' : vendor,
+            'location' : location,
+            'image' : image
+        }
+        DataSet.ORDERS.append(item)
+        return DataSet.ORDERS
+
+#=========================End Of Administrators Transactions==================================

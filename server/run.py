16f20657ca572ae28b201496f5f45cdc329edd9c
GransonO <oyombegranson@gmail.com>
2018-09-25 23:15:30 +0300

    [ch#160670603]Integrate Travis CI  for continuous testing and coveralls for code analysis

---
+++ server/run.py
@@ -0,0 +1,10 @@
+from flask import Flask
+from flask_restplus import Api
+
+from app.api.v1 import routes
+
+if __name__ == '__main__':
+    import os  
+    port = int(os.environ.get('PORT', 5000)) 
+    routes.app.run(host='0.0.0.0', port=port)
+    

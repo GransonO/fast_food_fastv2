37fbf1f07ec58e8e3e1af46301fd7060a0df8311
GransonO <oyombegranson@gmail.com>
2018-09-26 18:11:27 +0300

    [ft#160675064]Create post new order

--- server/app/api/v1/routes/__init__.py
+++ server/app/api/v1/routes/__init__.py
@@ -1,4 +1,4 @@
-from flask import Flask,Blueprint
+from flask import Flask,Blueprint,request
 from flask_restplus import Resource,Api,fields
 from app import create_app      #Used . to import from a top level package
 from ..services.data_handler import DataSet  #package for data manipulations
@@ -21,18 +21,29 @@ class All(Resource):
     def post(self):
         '''Adds a new item to the items list'''
         sent_data = api.payload
-        name = sent_data['name']
-        description = sent_data['Description']
-        quantity = sent_data['quantity']
-        price = sent_data['price']
-        vendor = sent_data['vendor']
-        location  = sent_data['location']
-        image = sent_data['image']
-        identifier = sent_data['identifier']
+        try:
+            if(sent_data == {}):
+                return {'data': 'You cant send an empty request'}
+            else:
 
-        result = DataSet.add_new_entry(self,name,description,quantity,price,vendor,location,image,identifier)
-        return {'data': result}
-        
+                name = sent_data['name']
+                description = sent_data['Description']
+                quantity = sent_data['quantity']
+                price = sent_data['price']
+                vendor = sent_data['vendor']
+                location  = sent_data['location']
+                image = sent_data['image']
+                identifier = sent_data['identifier']
+
+                result = DataSet.add_new_entry(self,name,description,quantity,price,vendor,location,image,identifier)
+                return {'data': result}    
+
+        except KeyError:
+            return {'data':'Please enter the data as specified'}
+
+        except:
+            return {'data':'Your data could not be posted, are you trying something clever?'} 
+       
 
 #For Specific
 class Specific(Resource):

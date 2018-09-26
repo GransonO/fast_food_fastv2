9425428f2f044e52f4774082a1c02bfb41802f7e
GransonO <oyombegranson@gmail.com>
2018-09-26 14:19:00 +0300

    [ch#160771585]Test all endpoints

--- server/run.py
+++ server/run.py
@@ -6,5 +6,5 @@ from app.api.v1 import routes
 if __name__ == '__main__':
     import os  
     port = int(os.environ.get('PORT', 5000)) 
-    routes.app.run(host='0.0.0.0', port=port)
+    routes.app.run(host='127.0.0.1', port=port)
     

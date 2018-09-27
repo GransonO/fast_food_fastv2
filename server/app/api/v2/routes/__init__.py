d5ab681a0ac8143e5ed760d8cbf7fd6cb8d7ec2b
GransonO <oyombegranson@gmail.com>
2018-09-27 18:28:43 +0300

    Registration prior

--- server/app/api/v2/routes/__init__.py
+++ server/app/api/v2/routes/__init__.py
@@ -1,7 +1,7 @@
 from flask import Flask
 from flask_restplus import Resource,Api,fields
 from app import create_app
-from ..services.db_handler import DataSet 
+from ..services.db_handler import ServiceSpace 
 from app.secret import all_secrets 
 
 app = create_app('Developing')
@@ -9,20 +9,76 @@ app = create_app('Developing')
 api = Api(app)
 
 delete_prop = api.model('Deleting an item',{'uid': fields.String('The users id(Must be an admin )')})
+auth_login_ = api.model('User Login',{'type': fields.String('The user can be either ADMIN or CUSTOMER'),'name': fields.String('The username registered'),'password': fields.String('The users password')})
+auth_sign_up = api.model('User Sign Up',{'type': fields.String('The user can be either ADMIN or CUSTOMER'),'name': fields.String('The username registered'),'vendor_name': fields.String('The if user is Admin'),'password': fields.String('The users password'),'about': fields.String('Brief Users description'),'location': fields.String('The users location'),'image_url': fields.String('The users uploaded image'),'phone_no': fields.String('The users phone number'),'email': fields.String('The users email')})
 
-#Authentication login and sign up
-class Authentication(Resource):
+#Authentication login
+class Auth_Login(Resource):
 
+    @api.expect(auth_login_)
     def post(self,action):
         '''Users Authentication'''
-        if(action == 'signup'):
-            return 'you have been signed up in'
-
-        elif(action == 'login'):
-            return 'You are logged in' 
+        if(action == 'login'):
+            sent_data = api.payload
+            Auth_Login.auth_verify(self,sent_data)
 
         else:
             return {'data':'Human! There is no action to be preformed here'}
+
+    def auth_verify(self,sent_data):
+        try:
+            if(sent_data == {}):
+                return {'data': 'You cant send an empty request'}
+            else:
+                type = sent_data['type']
+                name = sent_data['name']
+                password = sent_data['password']
+
+                result = ServiceSpace.retrieve_user(self,type,name,password)
+                return {'data': result}    
+
+        except KeyError:
+            return {'data':'Please enter the data as specified'}
+
+        except:
+            return {'data':'Your data could not be posted, are you trying something clever?'}
+
+#Authentication sign up
+class Auth_Sign_Up(Resource):
+
+    @api.expect(auth_sign_up)
+    def post(self,action):
+        '''Users Authentication'''
+        if(action == 'signup'):
+            sent_data = api.payload
+            Auth_Sign_Up.auth_verify(self,sent_data)
+
+        else:
+            return {'data':'Human! There is no action to be preformed here'}
+
+    def auth_verify(self,sent_data):
+        try:
+            if(sent_data == {}):
+                return {'data': 'You cant send an empty request'}
+            else:
+                type = sent_data['type']
+                name = sent_data['name']
+                password = sent_data['password']
+                vendor_name = sent_data['vendor_name']
+                phone_no  = sent_data['phone_no']
+                email = sent_data['email']
+                about = sent_data['about']
+                location = sent_data['location']
+                image_url = sent_data['image_url']
+
+                result = ServiceSpace.Add_user_to_db(self,type,name,password,vendor_name,phone_no,email,about,location,image_url)
+                return {'data': result}    
+
+        except KeyError:
+            return {'data':'Please enter the data as specified'}
+
+        except:
+            return {'data':'Your data could not be posted, are you trying something clever?'}
 
 #Users data Interactions
 class UsersOrders(Resource):
@@ -62,7 +118,8 @@ class RequestMenu(Resource):
 
 
 #Authentication
-api.add_resource(Authentication,'/auth/<string:action>')
+api.add_resource(Auth_Sign_Up,'/auth/signup')
+api.add_resource(Auth_Login,'/auth/login')
 
 #Users actions
 api.add_resource(UsersOrders,'/users/orders')

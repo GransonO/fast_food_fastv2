00f872e7263057d31cb9bf04247aab0e545a4c76
Granson Oyombe <oyombegranson@gmail.com>
2018-10-04 20:38:02 +0300

    [ch#160992442]Host application on Heroku

--- server/app/database/instantiate_db.py
+++ server/app/database/instantiate_db.py
@@ -40,7 +40,7 @@ class DatabaseBase():
 
         cur = base_creation(self,db_name)
         for table in tables:
-            print('Script : {}'.table)
+            print('Script : {}'.format(table))
             cur.execute(table)
         cur.close()
 

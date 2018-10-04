7451b5ba9f6f0de51dc0d360cf66a49e3c80fa8e
GransonO <oyombegranson@gmail.com>
2018-10-04 19:50:20 +0300

    [ch#160992442]Host application on Heroku

--- server/app/database/db_init.py
+++ server/app/database/db_init.py
@@ -1,10 +1,10 @@
 import psycopg2
 #Connects to the DB
 def base_creation(self,db_name):
-    Host = 'ec2-54-225-68-133.compute-1.amazonaws.com'
-    User = 'xbrpdqtjsmtxfp'
+    Host = 'ec2-75-101-153-56.compute-1.amazonaws.com'
+    User = 'zmpgcuupebdapt'
     Port = '5432'
-    Password = 'bdfdec3c6bfec300a06f78d99aa45fc7a3443425eb8a164013a6610a234cb92d'
+    Password = '2281a9c0b937637b819852eb171db14e790e4c74a544a101f12a604535697aea'
 
     try:
         conn = psycopg2.connect("dbname={} user={} password={} host={} port={}".format(db_name,User,Password,Host,Port))

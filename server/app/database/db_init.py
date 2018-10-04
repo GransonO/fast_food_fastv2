d5160dc2da327cdf873ad42d7c2654ba7fff9285
GransonO <oyombegranson@gmail.com>
2018-10-04 20:20:39 +0300

    [ch#160992442]Host application on Heroku

--- server/app/database/db_init.py
+++ server/app/database/db_init.py
@@ -4,6 +4,7 @@ def base_creation(self,db_name):
     Host = 'ec2-75-101-153-56.compute-1.amazonaws.com'
     User = 'zmpgcuupebdapt'
     Port = '5432'
+    db_name = 'd8cv78aggrjnp3'
     Password = '2281a9c0b937637b819852eb171db14e790e4c74a544a101f12a604535697aea'
 
     try:
--- server/app/database/instantiate_db.py
+++ server/app/database/instantiate_db.py
@@ -10,7 +10,7 @@ class DatabaseBase():
         if db_state == 'Production': #For production
             db_name = 'fast_food_db'
         else:
-            db_name = 'test_fast_food_db' #For testing
+            db_name = 'd8cv78aggrjnp3' #For testing
         return db_name
 
     
@@ -55,7 +55,7 @@ class DatabaseBase():
 
     #Calls each build function in order
     def order_of_creation(self,db_state):
-        DatabaseBase.drop_db(self,db_state) #Drop DB if exists
-        DatabaseBase.create_db(self,db_state) #Create DB
+        #DatabaseBase.drop_db(self,db_state) #Drop DB if exists
+        #DatabaseBase.create_db(self,db_state) #Create DB
         DatabaseBase.create_tables(self,db_state) #Create tables
         return 'Creation Success for state {}.'.format(db_state)

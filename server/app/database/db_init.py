beed26cdd65d2192864b53e2881966cb90353f7a
Granson Oyombe <oyombegranson@gmail.com>
2018-10-13 00:57:03 +0300

    Edited connection property to fit Herokus

--- server/app/database/db_init.py
+++ server/app/database/db_init.py
@@ -2,11 +2,11 @@ import psycopg2
 
 #Connects to the DB
 def base_creation(self,db_name):
-    Host = 'ec2-54-247-101-191.eu-west-1.compute.amazonaws.com'
-    db_name = 'd9fpabqvje4pfk'
-    User = 'awpvubyrjvurpb'
+    Host = 'ec2-46-51-184-229.eu-west-1.compute.amazonaws.com'
+    db_name = 'd2atgsnecjnp3c'
+    User = 'wywkzbvvmrhxiw'
     Port = '5432'
-    Password = 'bfad832890724724d4acf710faef3820fb5fa1423a60319438a33a1933a47554'
+    Password = 'dd1e98da7141ef01dbf9ebf05ce1225d91b96c12542a57a5bbb251dc324250ff'
     
     try:
         conn = psycopg2.connect("dbname={} user={} password={} host={} port={}".format(db_name,User,Password,Host,Port))
